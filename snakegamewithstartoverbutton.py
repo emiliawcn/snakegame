62281b0e554ae5dca7262ec57e1ecc50e282822a
tgasslander <tobias@gasslander.com>
2023-07-24 09:37:00 +0200

    play nice with i3wm

--- snakegamewithstartoverbutton.py
+++ snakegamewithstartoverbutton.py
@@ -150,8 +150,6 @@ screen_height = window.winfo_screenheight()
 x = int((screen_width/2) - (window_width/2))
 y = int((screen_height/2) - (window_height/2))
 
-window.geometry(f"{window_width}x{window_height}+{x}+{y}")
-
 window.bind('<Left>', lambda event: change_direction('left'))
 window.bind('<Right>', lambda event: change_direction('right'))
 window.bind('<Up>', lambda event: change_direction('up'))
